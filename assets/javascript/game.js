fad07c931009ce13fa64a5fcd1e6173b6f0cc717
Your Name <you@example.com>
2018-06-11 18:34:25 -0400

    3rd iteration

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,96 @@
 
+var isFirstGuess = 0;
 
-$(document).ready(function(){
+$("#pikachu").on("click", function() {
 
-    $("#pikachu").on("click", function() {
+    if (isFirstGuess === 0) {
 
-        $(".item5").css("visibility", "hidden");
-        });
+        $(".item1").removeClass(".item1").addClass("item5");
+        isFirstGuess++; 
+        $("#lede").html("Now Choose Your Enemy!");
+        $("#pikachuSound")[0].play();
 
- });
+
+
+    } else {
+        $(".item1").removeClass("item1").addClass("item6");
+        $("#lede").html("FIGHT");
+        $(".item7").css("visibility", "visible");
+        $("#pikachuSound")[0].play();
+
+
+        } 
+    
+});
+
+$("#onyx").on("click", function() {
+
+    if (isFirstGuess === 0) {
+
+    $(".item2").removeClass("item2").addClass("item5");
+    isFirstGuess++; 
+    $("#lede").html("Now Choose Your Enemy!");
+    $("#onyxSound")[0].play();
+    
+
+
+} else  {
+    $(".item2").removeClass("item2").addClass("item6");
+    $(".item7").css("visibility", "visible");
+    $("#lede").html("FIGHT!");  
+    isFirstGuess++; 
+
+}
+
+    });
+
+$("#magnemite").on("click", function() {
+
+    if (isFirstGuess === 0) {
+
+   $(".item3").removeClass("item3").addClass("item5");
+    $("#lede").html("Now Choose Your Enemy!");
+    isFirstGuess++; 
+    $("#magnemiteSound")[0].play();
+
+    } else {
+        $(".item3").removeClass("item3").addClass("item6");
+        $(".item7").css("visibility", "visible");
+        $("#lede").html("FIGHT!");
+        $("#magnemiteSound")[0].play();
+        isFirstGuess++; 
+        }
+    });
+
+
+$("#ivysaur").on("click", function() {
+
+
+    if (isFirstGuess === 0) {
+
+    $(".item4").removeClass("item4").addClass("item5");
+    $("#lede").html("Now Choose Your Enemy!");
+    isFirstGuess++; 
+    $("#ivysaurSound")[0].play();
+
+
+    } else {
+        $(".item4").removeClass("item4").addClass("item6");
+        $("#lede").html("FIGHT!");
+        $(".item7").css("visibility", "visible");
+        isFirstGuess++; 
+        $("#ivysaurSound")[0].play();
+
+    }
+
+
+    });
+
+
+
+ 
+
+
+
+
 
